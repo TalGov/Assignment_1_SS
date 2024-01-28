7f505b018b8ea0ea1d2008b22742f374471981e1
Y.P <pardes.yoyo@gmail.com>
2024-01-28 16:18:23 +0200

    V2.3

--- main.c
+++ main.c
@@ -8,19 +8,19 @@ int main()
     int minNum;
     int maxNum;
 
-    printf("\n Please enter the first number:");
+    // printf("\n Please enter the first number:");
     scanf("%d", &number1);
     while (number1 < 0)
     {
-        printf("\n Error! Please enter the first number again:");
+        // printf("\n Error! Please enter the first number again:");
         scanf("%d", &number1);
     }
 
-    printf("\n Please enter the second number:");
+    // printf("\n Please enter the second number:");
     scanf("%d", &number2);
     while (number2 < 0)
     {
-        printf("\n Error! Please enter the second number again:");
+        // printf("\n Error! Please enter the second number again:");
         scanf("%d", &number2);
     }
 
